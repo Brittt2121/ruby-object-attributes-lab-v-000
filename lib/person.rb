25247d5f49dff48e3aecc0fee987bc91c2e24238
Brittany Bellantoni <brittt2121@hotmail.com>
2018-02-16 16:20:19 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,23 +1,20 @@
 class Person
 
-  def initialize(persons_name)
+  def initialize(persons_name, persons_job)
     @this_persons_name = persons_name
+    @this_persons_job = persons_job
   end
 
   def name
     @this_persons_name
   end
 
-  def name=(new_name)
-    @this_persons_name = new_name
-  end
-
-  def job=(persons_job)
-    @this_persons_job = persons_job
-  end
-
   def job
     @this_persons_job
+  end
+
+  def name=(new_name)
+    @this_persons_name = new_name
   end
 
   def job=(new_job)
